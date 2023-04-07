300f95db5f2cec2838ea34b39ad568529aebbb36
Ranieeery <raniery2003@hotmail.com>
2023-04-07 19:37:01 -0300

    :tada: init - start.sprint.io first commit

---
+++ src/test/java/br/com/correios/api/CorreiosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.correios.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreiosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
